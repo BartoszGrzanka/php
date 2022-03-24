a2cdabe5d536036f056bb153f9e1c4f072858968
BartoszGrzanka <91725411+BartoszGrzanka@users.noreply.github.com>
2022-03-24 13:42:29 +0100

    missing 3.3

--- zadania3/Zadanie3.2.php
+++ zadania3/Zadanie3.2.php
@@ -1,8 +1,34 @@
 <?php
-function nationality($nation){
-    $nations=["Polska"=>"Polak","Hiszpania"=>"Hiszpan"];
-    return $nations[$nation];
-}
+echo '<form action="Zadanie3.2.php" method="get">
+  <label for="fnumber">First number:</label><br>
+  <input type="number" name="fnumber" required><br>
+    <input type="submit" name="submit" value="Submit">
+</form>';
 
-$nation="Hiszpania";
-echo nationality($nation);
+
+function primeNumber2($n){
+    $iterator=1;
+    if($n<=1){
+        return "Not Prime".$iterator;
+    }
+    if($n==2){
+        return "Prime".$iterator;
+    }
+    if($n%2==0){
+        return "Not Prime ".$iterator;
+    }
+
+    $ceil = ceil(sqrt($n));
+
+    for($i = 3; $i <= $ceil; $i = $i + 2) {
+        if($n % $i == 0){
+            return "Not Prime".$iterator;
+        }
+        $iterator++;
+    }
+    return "Prime".$iterator;
+}
+if(isset($_GET['submit'])){
+        echo primeNumber2($_GET['fnumber']);
+
+}
