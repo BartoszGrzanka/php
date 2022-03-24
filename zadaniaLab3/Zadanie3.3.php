6e6cc8586200015b68ed6878e05000839197d6ed
BartoszGrzanka <91725411+BartoszGrzanka@users.noreply.github.com>
2022-03-24 14:37:16 +0100

    Add files via upload

--- zadaniaLab3/Zadanie3.3.php
+++ zadaniaLab3/Zadanie3.3.php
@@ -7,6 +7,12 @@ echo '<form action="Zadanie3.3.php" method="get">
   <label for="dateEnd">Date end:</label><br>
   <input type="date" name="dateEnd" required><br>
   
+  <label for="country">Kraj:</label><br>
+  <select name="country">
+  <option value="Polska">Polska</option>
+  <option value="Hiszpania">Hiszpania</option>
+</select><br>
+  
   <label for="personQuantity">Osoby:</label><br>
   <select name="personQuantity">
   <option value="1">1</option>
@@ -18,9 +24,24 @@ echo '<form action="Zadanie3.3.php" method="get">
   <input type="submit" name="submit" value="Submit">
 </form>';
 
+/*$tariff = array (
+    array("Poland",30,18),
+);*/
+$tariff=["Polska"=>30,"Hiszpania"=>50];
+
 if(isset($_GET['submit'])){
     $personQuantity=$_GET['personQuantity'];
     $dateStart=$_GET['dateStart'];
     $dateEnd=$_GET['dateEnd'];
+    $country=$_GET['country'];
+    //echo $country;
+    if($dateEnd>$dateStart){
+        
+
+    }
+    else{
+        echo "Error: nie możesz zakończyć podruży przed jej startem";
+    }
+
 
 }
